1fd06f8f53f0082811bfecd14d6c195d2331531d
semicolon guy <itworksnavneet@gmail.com>
2026-02-05 22:30:34 +0530

    request details: all about details we will be getting

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -42,6 +42,26 @@ func main() {
 	http.HandleFunc("/students", func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
 		case http.MethodGet:
+
+			// Access request details
+			fmt.Println("Body:", r.Body)                          // &{0x1400000e1e0 <nil> <nil> false true {{} {0 0}} true false false 0x1009dba40}
+			fmt.Println("Form:", r.Form)                          // map[]
+			fmt.Println("Header:", r.Header)                      //  map[Accept:[*/*] Accept-Encoding:[gzip, deflate, br] Connection:[keep-alive] Content-Length:[63] Content-Type:[application/json] Postman-Token:[92a452d3-bf22-4baf-80d0-dabd65b9fb58] User-Agent:[PostmanRuntime/7.51.1]]
+			fmt.Println("Context:", r.Context())                  // context.Background.WithValue(net/http context value http-server, *http.Server).WithValue(net/http context value local-addr, [::1]:3000).WithCancel.WithCancel
+			fmt.Println("Content Len:", r.ContentLength)          // 63
+			fmt.Println("Host:", r.Host)                          // localhost:3000
+			fmt.Println("Method:", r.Method)                      // POST
+			fmt.Println("Protocol:", r.Proto)                     // HTTP/1.1
+			fmt.Println("Remote addr:", r.RemoteAddr)             // [::]:58871
+			fmt.Println("Req URI:", r.RequestURI)                 // /students
+			fmt.Println("TLS:", r.TLS)                            // <nil>
+			fmt.Println("Trailer:", r.Trailer)                    // map[]
+			fmt.Println("Transfer Encoding:", r.TransferEncoding) // []
+			fmt.Println("URL:", r.URL)                            // /students
+			fmt.Println("User agent:", r.UserAgent())             // PostmanRuntime/7.51.1
+			fmt.Println("Port in URL:", r.URL.Port())             //
+			fmt.Println("Scheme in URL:", r.URL.Scheme)           //
+
 			_, err := w.Write([]byte("GET: Students Route"))
 			if err != nil {
 				log.Fatal("Error writing to server:", err)
