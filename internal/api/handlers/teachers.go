532857e66591bd85b93c7cafb02fdf0eabd2298c
semicolon guy <itworksnavneet@gmail.com>
2026-03-19 17:59:38 +0530

    update post method for teachers

--- internal/api/handlers/teachers.go
+++ internal/api/handlers/teachers.go
@@ -6,15 +6,15 @@ import (
 	"net/http"
 	"strconv"
 	"strings"
-	"sync"
 
 	"restapi/internal/models"
+	"restapi/internal/repository/sqlconnect"
 )
 
 var (
 	teachers = make(map[int]models.Teacher)
-	mutex    = &sync.Mutex{}
-	nextID   = 1
+	// mutex    = &sync.Mutex{}
+	nextID = 1
 )
 
 // initialize some data
@@ -106,20 +106,48 @@ func getTeachersHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func addTeacherHandler(w http.ResponseWriter, r *http.Request) {
-	mutex.Lock()
-	defer mutex.Unlock()
+	db, err := sqlconnect.ConnectDB()
+	if err != nil {
+		http.Error(w, "Error connecting to database", http.StatusInternalServerError)
+		return
+	}
+	defer db.Close()
 
 	var newTeachers []models.Teacher
 	if err := json.NewDecoder(r.Body).Decode(&newTeachers); err != nil {
 		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
 	}
+
+	// addedTeachers := make([]models.Teacher, len(newTeachers))
+	// for i, newTeacher := range newTeachers {
+	// 	newTeacher.ID = nextID
+	// 	teachers[nextID] = newTeacher
+	// 	addedTeachers[i] = newTeacher
+	// 	nextID++
+	// }
+
+	stmt, err := db.Prepare("INSERT INTO teachers (first_name, last_name, email, class, subject) VALUES (?, ?, ?, ?, ?)")
+	if err != nil {
+		http.Error(w, "Error preparing SQL query", http.StatusInternalServerError)
+		return
+	}
+	defer stmt.Close()
 
 	addedTeachers := make([]models.Teacher, len(newTeachers))
 	for i, newTeacher := range newTeachers {
-		newTeacher.ID = nextID
-		teachers[nextID] = newTeacher
+		res, err := stmt.Exec(newTeacher.FirstName, newTeacher.LastName, newTeacher.Email, newTeacher.Class, newTeacher.Subject)
+		if err != nil {
+			http.Error(w, "Error inserting data into db", http.StatusInternalServerError)
+			return
+		}
+		lastID, err := res.LastInsertId()
+		if err != nil {
+			http.Error(w, "Error getting the last id", http.StatusInternalServerError)
+			return
+		}
+		newTeacher.ID = int(lastID)
 		addedTeachers[i] = newTeacher
-		nextID++
 	}
 
 	w.Header().Set("Content-Type", "application/json")
